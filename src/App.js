88985095b918feffc6485f6deaf629ed4b9508d5
Bruno <brubarratella@hotmail.com>
2023-05-22 15:18:42 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import SayMyName from './components/SayMyName'
+import Pessoa from './components/Pessoa'
+import Frase from './components/Frase'
+import List from './components/List'
 
-function App() {
+
+
+function App(){
+  const nome = 'Maria'
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Testando Css</h1>
+      <Frase />
+      <Frase />
+      <SayMyName nome="Matheus"/>
+      <SayMyName nome="João"/>
+      <SayMyName nome={nome}/>
+      <Pessoa 
+      nome="Rodrigo"
+      idade="28"
+      profissao="Programador"
+      foto="https://via.placeholder.com/150"
+      />
+      <List />
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
